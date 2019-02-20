8a7c5703f7a98499aa066b7a55a22f0d29b61132
jiangtj <116749895@qq.com>
2019-02-20 10:47:46 +0800

    Add netlify test

---
+++ netlify-auto-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+npm install hexo-cli -g
+git clone https://github.com/JiangTJ/blog.git blog
+cd blog
+mv ../languages themes/next/
+mv ../layout themes/next/
+mv ../scripts themes/next/
+mv ../source themes/next/
+mv ../_config.yml themes/next/
+mv ../package.json themes/next/
+npm install
+hexo config theme next
+hexo g
+mv public ../
