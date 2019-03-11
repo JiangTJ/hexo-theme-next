0baafe53b65fa0bc1327692047c089678d0f65e6
jiangtj <116749895@qq.com>
2019-03-11 17:19:41 +0800

    R

--- scripts/filters/lazyload.js
+++ scripts/filters/lazyload.js
@@ -8,27 +8,40 @@
  *   enable: true/false
  *   max_width:
  */
-hexo.extend.filter.register('after_post_render', function (data) {
-  var theme = hexo.theme.config;
+// hexo.extend.filter.register('after_post_render', function (data) {
+//   var theme = hexo.theme.config;
 
-  if (!theme.lazyload) return;
+//   if (!theme.lazyload) return;
 
-  var cheerio;
+//   var cheerio;
 
-  if (!cheerio) cheerio = require('cheerio');
+//   if (!cheerio) cheerio = require('cheerio');
 
-  var $ = cheerio.load(data.content, { decodeEntities: false });
+//   var $ = cheerio.load(data.content, { decodeEntities: false });
 
+//   $('img').each(function () {
+
+//     let $image = $(this);
+//     let imageLink = $image.attr('src');
+//     $image.attr('data-src', imageLink);
+//     //$image.removeAttr('src');
+//     $image.attr('src', '/images/loading-l.gif');
+//     $image.attr('class',  $image.attr('class')||'lazyload');
+
+//   });
+
+//   data.content = $.html();
+// }, 1);
+
+function doLazyload ($) { 
   $('img').each(function () {
-
     let $image = $(this);
     let imageLink = $image.attr('src');
     $image.attr('data-src', imageLink);
     //$image.removeAttr('src');
     $image.attr('src', '/images/loading-l.gif');
     $image.attr('class',  $image.attr('class')||'lazyload');
-
   });
+}
 
-  data.content = $.html();
-}, 1);
+module.exports = doLazyload;
--- scripts/filters/netlify-lm.js
+++ scripts/filters/netlify-lm.js
@@ -8,16 +8,69 @@
  *   enable: true/false
  *   max_width:
  */
-hexo.extend.filter.register('after_post_render', function (data) {
-  var theme = hexo.theme.config;
+// hexo.extend.filter.register('after_render:html', function (data) {
+//   var theme = hexo.theme.config;
 
-  if (!theme.netlify_lm.enable) return;
+//   if (!theme.netlify_lm) return;
 
-  var cheerio;
+//   if (!theme.netlify_lm.enable) return;
 
-  if (!cheerio) cheerio = require('cheerio');
+//   var cheerio;
 
-  var $ = cheerio.load(data.content, { decodeEntities: false });
+//   if (!cheerio) cheerio = require('cheerio');
+
+//   var $ = cheerio.load(data, { decodeEntities: false });
+
+//   $('head').each(function () {
+//     let $head = $(this);
+//     $head.append('<script src="https://cdn.jsdelivr.net/npm/lozad@1.9.0/dist/lozad.min.js" integrity="sha256-50cmb3K6Zka/WMfXLFzqyo5+P+ue2JdsyEmSEsU58s4=" crossorigin="anonymous"></script>\n');
+//   });
+
+//   $('img').each(function () {
+
+//     let $image = $(this);
+//     let imageLink = $image.attr('src');
+//     if (!imageLink || imageLink.indexOf('nf_resize=') >= 0) return;
+
+
+//     let width = $image.attr('width');
+//     let height = $image.attr('height');
+//     let whList = [];
+//     if(width){
+//       whList.push('w=' + width);
+//     }
+//     if(height){
+//       whList.push('h=' + height);
+//     }
+//     if (whList.length > 0) {
+//       if (imageLink.indexOf('?') < 0) {
+//         imageLink += '?';
+//       }
+//       $image.attr('src', imageLink + "nf_resize=fit&" + whList.join("&"));
+//       return;
+//     }
+
+//     if (theme.netlify_lm.max_width) {
+//       if (imageLink.indexOf('?') < 0) {
+//         imageLink += '?';
+//       }
+//       $image.attr('src', imageLink + "nf_resize=fit&w=" + theme.netlify_lm.max_width);
+//     }
+//   });
+
+//   return $.html();
+// });
+
+function doNetlifyLM ($) { 
+
+  // $('head').each(function () {
+  //   let $head = $(this);
+  //   $head.append('<script src="https://cdn.jsdelivr.net/npm/lozad@1.9.0/dist/lozad.min.js" integrity="sha256-50cmb3K6Zka/WMfXLFzqyo5+P+ue2JdsyEmSEsU58s4=" crossorigin="anonymous"></script>\n');
+  // });
+
+  let config = {
+    maxWidth: null
+  };
 
   $('img').each(function () {
 
@@ -43,13 +96,14 @@ hexo.extend.filter.register('after_post_render', function (data) {
       return;
     }
 
-    if (theme.netlify_lm.max_width) {
+    if (config.maxWidth) {
       if (imageLink.indexOf('?') < 0) {
         imageLink += '?';
       }
-      $image.attr('src', imageLink + "nf_resize=fit&w=" + theme.netlify_lm.max_width);
+      $image.attr('src', imageLink + "nf_resize=fit&w=" + config.maxWidth);
     }
   });
 
-  data.content = $.html();
-}, 0);
+}
+
+module.exports = doNetlifyLM;
