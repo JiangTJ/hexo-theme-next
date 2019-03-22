e3bffa13f08a4141141ed06ad418bef74c2af233
MrTT <jiang.taojie@foxmail.com>
2019-03-22 23:02:48 +0800

    webp c

--- scripts/console/pic.js
+++ scripts/console/pic.js
@@ -3,29 +3,61 @@ const imageminWebp = require('imagemin-webp');
 const fs = require('fs');
 var path = require('path');
 
-function webpAllPic (root) {
-    fs.readdir(root,{
+function webpAllPic(root) {
+    fs.readdir(root, {
         withFileTypes: true
-    },function (err, files) {
+    }, function (err, files) {
         if (err) {
             console.error(err);
             return;
         }
         //console.log(files);
         files.forEach(item => {
-            let temp = path.join(root,item.name);
+            let temp = path.join(root, item.name);
             if (item.isFile()) {
-                let pic = new RegExp('(.jpeg|.png|.jpg)$','gi');
+                let pic = new RegExp('(.jpeg|.png|.jpg)$', 'gi');
                 if (pic.test(item.name)) {
                     //console.error(temp);
                     imagemin([temp], root, {
-                        use: [ imageminWebp({quality: 75}) ]
+                        use: [imageminWebp({ quality: 75 })]
                     }).then(() => {
                         console.log('Images optimized: ' + temp);
                     });
                 }
             } else {
                 webpAllPic(temp);
+            }
+        });
+    });
+}
+
+function cleanPic(root) {
+    fs.readdir(root, {
+        withFileTypes: true
+    }, function (err, files) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        files.forEach(item => {
+            let temp = path.join(root, item.name);
+            if (item.isFile()) {
+                let pic = new RegExp('(.jpeg|.png|.jpg)$', 'gi');
+                if (pic.test(item.name)) {
+                    let end = temp.lastIndexOf('.');
+                    fs.exists(temp.substring(0, end) + '.webp', (exists) => {
+                        if (exists) {
+                            fs.unlink(temp, (err) => {
+                                if (err) throw err;
+                                console.log('Deleted: ' + temp);
+                            });
+                        } else {
+                            console.warn('webp not exist: ' + temp);
+                        }
+                    });
+                }
+            } else {
+                cleanPic(temp);
             }
         });
     });
@@ -40,8 +72,14 @@ hexo.extend.console.register('webp', '压缩', {
     let path = "";
 
     // all
-    if (args.all || args.a) {
-        webpAllPic('source');
+    if (args.a || args.all) {
+        webpAllPic('source/_posts');
+        return;
+    }
+
+    // clean
+    if (args.c || args.clean) {
+        cleanPic('source/_posts');
         return;
     }
 
@@ -49,7 +87,7 @@ hexo.extend.console.register('webp', '压缩', {
     if (args.images) {
         path = 'source/images';
     }
-    
+
     // post path
     if (args._[0]) {
         path = 'source/_posts/' + hexo.config.post_base_dir + '/' + args._[0];
@@ -59,7 +97,7 @@ hexo.extend.console.register('webp', '压缩', {
     if (args.path) {
         path = args.path;
     }
-    
+
     // custom type
     let type = '/*';
     if (args.type) {
@@ -74,7 +112,7 @@ hexo.extend.console.register('webp', '压缩', {
     //webp
     imagemin([path + type], path, {
         use: [
-            imageminWebp({quality: 75})
+            imageminWebp({ quality: 75 })
         ]
     }).then(() => {
         console.log('Images optimized');
