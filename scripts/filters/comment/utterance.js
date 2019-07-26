045f7e43a4d2a0186e5703d838a578cb748fb186
jiangtj <116749895@qq.com>
2019-07-26 18:43:03 +0800

    fix

--- scripts/filters/comment/utterance.js
+++ scripts/filters/comment/utterance.js
@@ -6,7 +6,7 @@ hexo.extend.filter.register('theme_inject', function(injects) {
   let theme = hexo.theme.config;
   if (!theme.utteranc.enable) return;
 
-  injects.bodyEnd.raw('utteranc', `
+  injects.comment.raw('utteranc', `
   {% if page.comments %}
   <script src="https://utteranc.es/client.js"
         repo="{{ theme.utteranc.repo }}"
